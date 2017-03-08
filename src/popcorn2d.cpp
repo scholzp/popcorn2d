9c2177a9c3d02fd1eb8c29e61f1f5e0c789f9a17
akrtur <pasix@outlook.de>
2017-03-08 14:11:17 +0100

    added setting to alter talpha to do testloops with different talpha for each loop

--- src/popcorn2d.cpp
+++ src/popcorn2d.cpp
@@ -1,23 +1,36 @@
 #include "ppm.hpp"
 #include "CsvWriter.h"
-
-#if defined(__PGI) or defined(__PGIC__)
-#include <openacc.h>
-#endif
-
 #include <sstream>
 #include <iostream>
 #include <chrono>
 #include <time.h>
 #include <cinttypes>
 #include <math.h>
+
+#if defined(__PGI) or defined(__PGIC__)
+#include <openacc.h>
+#endif
+
+
 #define PI 3.14159265359
-#define VERBOSE 1
-#define numberOfTests 20 //sets number of performed tests with given resolution
-#define numberOfRescalings 3 //sets number of resolution rescalings
+#define VERBOSE 0
+
+/*test setup
+ * Total Number of performed tests: numberofTest*talphaCount*numberOfRescaling
+ * Number of tests performed with set talpha: numberOfTests
+ * Number of tests performed with set resolution: numberOfTest*talphaCount
+ */
+
+#define numberOfTests 6 //sets number of performed tests with given resolution
+#define numberOfRescalings 5 //sets number of resolution rescalings
 #define EnableSafedialog 0 /*controlls if safedialog is displayed or skipped
 							 if skipped, picture wont be safed to file*/
+// dispersion settings
+#define talphaStart  0.0632   // sets start value for dispersion
+#define talphaIncrement  0.0075		// sets values by wich talpha is incremented
+#define talphaCount  5				// sets how often talpha is incremented
 using namespace std;
+
 //image settings
 const uint32_t ITERATION = 64;
 const uint32_t RES_EXPANSION = 200; //expands image resolution for each test
@@ -26,17 +39,13 @@ uint32_t HEIGHT = 0;
 uint32_t IMG_SIZE = WIDTH * HEIGHT;
 
 //parameters
-const int passCount = 2;
-const float s = 5.0, q = -5.0, l = 5.0, p = -5.0;
+const float s = 5.0, q = -5.0, l = 5.0, p = -5.0; //sets zoom
 uint32_t w = WIDTH;
 uint32_t h = HEIGHT;
 const float t0 = 31.1;
 const float t1 = -43.4;
 const float t2 = -43.3;
 const float t3 = 22.2;
-float talphaStart = 0.0632;
-
-
 /**
  * Set color values for pixel (i,j).
  * Underlying format is flattened structure of arrays
@@ -47,7 +56,6 @@ template<typename T>
 void colorImage(T* image) {
 	//color order ist r, g, b
 	float colors[3];
-
 	for (uint32_t y=0; y  <  HEIGHT; ++y) {
 	 for (uint32_t x=0; x  <  WIDTH; ++x) {
 	      float density = sqrt(image[x + y*WIDTH ]);
@@ -90,14 +98,8 @@ void initImage(T* image){
 	}
 }
 
-
-
-
 template<typename T>
-void computeImage(T* image) {
-  float talpha = talphaStart;
-
-  {
+void computeImage(T* image, float talpha) {
 #pragma acc parallel loop independent
     for (uint32_t y = 0; y < HEIGHT; ++y) {
 #pragma acc loop independent
@@ -125,10 +127,6 @@ void computeImage(T* image) {
     	// color pixels by generated values
 #endif
     }
-  }
-
-
-
 }
 
 char * getFileName(char *dst, int ext){
@@ -169,16 +167,16 @@ int main(void) {
   char buffer[17];
   char in;
   int flag = 0;
-  int log[numberOfTests + 2];//Height,Width,Test1,Test2...,Testn
+  float log[numberOfTests + 3];//Height,Width,talpha,Test1,Test2...,Testn
+  float dispersion = 0;
   //Output file
   std::CsvWriter Output;
   //generating first line for CSV-File (headline)
   std::stringstream sstr;
-  sstr << "Width, Height,";
+  sstr << "Width, Height, talpha,";
   for (int count = 0; count < numberOfTests; count++ ){
   	  sstr << "Test"<<count<<",";
   }
-  //sstr<<"\n";
   Output.addLineString(sstr.str());
   //the image is computed numberOfRescaling*numberOfTest-Times
   for (int rescaleCount = 0; rescaleCount <= numberOfRescalings; ++rescaleCount) {
@@ -193,36 +191,36 @@ int main(void) {
   	if (numberOfTests < 1){
   		return 100;
   	}
-  	// testNumber<0 => warmup no picture is saved as file
-  	for(int testNumber = -1; testNumber < numberOfTests; testNumber++){
-  		//Initialazition
-  	  initImage(image);
-  	  log[0] = WIDTH;
-  	  log[1] = HEIGHT;
-  	#pragma acc data copy(image[0:IMG_SIZE])
-  		auto start_time = chrono::steady_clock::now();
-  		//actuall computation
-  		for(int pass = 0; pass < passCount; ++pass){
-  #if VERBOSE==1
-  			std::cout << "Pass " << (pass+1) << " out of " << passCount << endl;
-  #endif
-  			computeImage(image);
-  			talphaStart += 0.001;
-  		}
-  	  auto end_time = chrono::steady_clock::now();
-  	  colorImage(image);
-  	  if(testNumber<0) // warmup
-  		continue;
-  		log[testNumber+2] = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
+  	//set height and with in log
+  	log[0] = WIDTH;
+  	log[1] = HEIGHT;
+  	for(int count = 0; count < talphaCount; ++count){
+  		//increment talpha
+		if ( count > -1){
+			dispersion = talphaStart + count * talphaIncrement;
+		}
+		//write talpha to log
+		log[2] = dispersion;
+		//compute image numberOfTests-times + 1 warmup
+		for(int testNumber = -1; testNumber < numberOfTests; testNumber++){
+			initImage(image);
+			#pragma acc data copy(image[0:IMG_SIZE])
+			auto start_time = chrono::steady_clock::now();
+			computeImage(image, dispersion);
+			auto end_time = chrono::steady_clock::now();
+			colorImage(image);
+			if(testNumber<0) // warmup
+				continue;
+			log[testNumber + 3] = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
+		}
+		for(int testNumber = 0; testNumber < numberOfTests; testNumber++)
+		{
+		cout <<"Test "<< testNumber+1 <<" executed in "<< log[testNumber+3] << " ms; Resolution "<<WIDTH<<"x"<<HEIGHT<<" talpha "<<log[2]<< endl;
+		}
+		Output.addLineValues(log, numberOfTests + 3);
+		dispersion = 0;
   	}
-  	//Output data from log-array
-  	for(int logCount = 0; logCount < numberOfTests; logCount++) {
-  	  cout <<"Test "<< logCount+1 <<" executed in "<< log[logCount+2] << " ms; Resolution "<<WIDTH<<"x"<<HEIGHT<< endl;
-  	}
-  delete[] image;
-	cout << "Tests for Resolution "<< WIDTH<<"x"<<HEIGHT<<" finished!\n";
-	Output.addLineValues(log, numberOfTests + 2);
-
+  	delete[] image;
   }
 #if EnableSafedialog == 1
   getFileName(buffer, 1);
@@ -242,6 +240,5 @@ int main(void) {
   getFileName(buffer, 0);
   Output.writeToCSV(buffer);
   //exit programm
-
   return 0;
 }
